e94ffc72019009b31b41926cb8f082069958faca
Paz777 <111736251+Paz777@users.noreply.github.com>
2023-11-06 10:26:55 +0000

    more on callbacks and rest parameters

--- assignment.js
+++ assignment.js
@@ -1,5 +1,30 @@
-function sayHello(name) {
-  console.log('Hi ' + name);
+const sayHello1 = () => 'Hi Georgey';
+
+const sayHello2 = (name, greet = 'hello') => console.log(greet + ' ' + name);
+
+const sayHello3 = name => 'Hi ' + name;
+
+const checkInput = (wordsHandler, ...words) => {
+  let isEmpty = false;
+  for (const word of words) {
+    if (!word) {
+      isEmpty = true;
+    }
+  }
+  if (!isEmpty) {
+    wordsHandler(words)
+  }
+};
+
+const outputWords = (words) => {
+  console.log(words);
 }
 
-sayHello();
+console.log(sayHello1('Pappi1'));
+sayHello2('Pappi2');
+console.log(sayHello3('Pappi3'));
+
+checkInput(() => {
+  console.log("No words are empty");
+}
+  , 'Pappi', 'is', 'happy', 'today', 'yeah')
